35feff922c64cb6cd4f729bce95a7176e821606b
Manuel Cañizares Ferrandiz <manuel@MacBook-Pro-de-Manuel.local>
2024-06-19 09:44:09 +0200

    Primer commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById('dropdown-products-content').classList.toggle('show');
+    document.getElementById('chevron-up').classList.toggle('show-2');
+}
+
