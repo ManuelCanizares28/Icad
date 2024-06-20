3dfcdc7d7996f9dc765ba1ced7c9c03b8f2f5c93
Manuel Cañizares Ferrandiz <manuel@MacBook-Pro-de-Manuel.local>
2024-06-20 13:49:46 +0200

    Descripción de los cambios realizados

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function myFunction() {
-    document.getElementById('dropdown-products-content').classList.toggle('show');
-    document.getElementById('chevron-up').classList.toggle('show-2');
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const productsButton = document.getElementById('products-button');
+    const productsContent = document.getElementById('dropdown-products-content');
+    const businessButton = document.getElementById('button-business');
+    const businessContent = document.getElementById('dropdown-business');
 
-function businessFunction() {
-    document.getElementById('dropdown-business').classList.toggle('show-business');
-}
+    productsButton.addEventListener('click', function() {
+        if(productsContent.style.display === 'none' || productsContent.style.display === '') {
+        productsContent.style.display = 'block';
+        productsButton.classList.add('active');
+        } else {
+            productsContent.style.display = 'none';
+            businessContent.style.display = 'none';
+            productsButton.classList.remove('active');
+        }
+    });
+
+    businessButton.addEventListener('click', function() {
+        if(businessContent.style.display === 'none' || businessContent.style.display === '') {
+        businessContent.style.display = 'block';
+        } else {
+            businessContent.style.display = 'none';
+        }
+    });
+});
+
