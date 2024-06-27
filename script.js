3f0e91bcb1d7a9c67173e53206609ccbb5690f4c
Manuel Cañizares Ferrandiz <manuel@MacBook-Pro-de-Manuel.local>
2024-06-27 11:37:58 +0200

    Cambios

--- script.js
+++ script.js
@@ -3,14 +3,27 @@ document.addEventListener('DOMContentLoaded', function() {
     const productsContent = document.getElementById('dropdown-products-content');
     const businessButton = document.getElementById('button-business');
     const businessContent = document.getElementById('dropdown-business');
+    const modelingButton = document.getElementById('modeling-button');
+    const modelingContent = document.getElementById('container-modeling');
+    const businessContainer = document.getElementById('container-business');
+    const productionButton = document.getElementById('production-button');
+    const lineaVertical = document.getElementById('linea-vertical');
+    const designButton = document.getElementById('design-button');
+    const designContent = document.getElementById('container-design');
+    const verticalModeling = document.getElementById('vertical-modeling');
+    const verticalDesign = document.getElementById('vertical-design');
 
     productsButton.addEventListener('click', function() {
         if(productsContent.style.display === 'none' || productsContent.style.display === '') {
         productsContent.style.display = 'block';
+        lineaVertical.style.display = 'block';
         productsButton.classList.add('active');
         } else {
             productsContent.style.display = 'none';
+            modelingContent.style.display = 'none';
             businessContent.style.display = 'none';
+            businessContainer.style.display = 'none';
+            designContent.style.display = 'none';
             productsButton.classList.remove('active');
         }
     });
@@ -18,9 +31,37 @@ document.addEventListener('DOMContentLoaded', function() {
     businessButton.addEventListener('click', function() {
         if(businessContent.style.display === 'none' || businessContent.style.display === '') {
         businessContent.style.display = 'block';
-        } else {
-            businessContent.style.display = 'none';
+        businessContainer.style.display = 'block';
         }
     });
+
+    modelingButton.addEventListener('click', function() {
+        if(modelingContent.style.display === 'none' || modelingContent.style.display === '') {
+            modelingContent.style.display = 'block';
+            businessContainer.style.display = 'none';
+            lineaVertical.style.display = 'none';
+            designContent.style.display = 'none';
+        }
+    });
+
+    productionButton.addEventListener('click', function() {
+        if(businessContainer.style.display === 'none' || businessContainer.style.display === '') {
+            businessContainer.style.display = 'block';
+            lineaVertical.style.display = 'block';
+            modelingContent.style.display = 'none';
+            designContent.style.display = 'none';
+        }
+    });
+    
+    designButton.addEventListener('click', function() {
+        if(designContent.style.display === 'none' || designContent.style.display === '') {
+            designContent.style.display = 'block';
+            modelingContent.style.display = 'none';
+            businessContainer.style.display = 'none';
+            lineaVertical.style.display = 'none';
+            verticalModeling.style.display = 'none';
+        }
+    });
+
 });
 
